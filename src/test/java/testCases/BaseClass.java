d066133d31e17b396792496bb4d29df2e2b53fc0
jaradamessa <67838264+jaradamessa@users.noreply.github.com>
2024-03-30 11:36:41 -0500

    Update BaseClass.java

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -33,7 +33,7 @@ public class BaseClass {
 			DesiredCapabilities cap = new DesiredCapabilities();
 			cap.setPlatform(Platform.LINUX);	
 			cap.setBrowserName("chrome");
-			driver = new RemoteWebDriver(new URL("http://18.207.217:4444"),cap);
+			driver = new RemoteWebDriver(new URL("http:// 18.207.217.4:4444"),cap);
 			/*
 			 * DesiredCapabilities cap = new DesiredCapabilities();
 			 * cap.setPlatform(Platform.WIN11); cap.setBrowserName("chrome"); driver = new
